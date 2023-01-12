247d3f1a6734efbc1870a253499675289cac3afb
kpias760 <kpiasecki760@gmail.com>
2023-01-12 16:49:26 +0100

    top bar with map name needs fix

--- src/components/CreatorHeader.js
+++ src/components/CreatorHeader.js
@@ -2,6 +2,7 @@ import logoTextMode from '../HBSE_files/top-tools/top-tools_text.png';
 import logoProperties from '../HBSE_files/top-tools/top-tools_pr.png';
 import logoHelp from "../HBSE_files/top-tools/top-tools_help.png";
 import $ from 'jquery'
+import "../App.css"
 
 var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
 var meta = {
@@ -135,10 +136,10 @@ function CreatorHeader(props) {
   return (
     <tr>
       <td>
-        <table className="underline" style={{ width: "100%" }}>
+        <table className="underline" id="creator-header">
           <tbody>
             <tr>
-              <td style={{ width: "100%" }} id="right_tabs">
+              <td id="right_tabs">
                 <input id="input_name" value={props.stadium ? props.stadium.name : ''} onChange={handleChange} autoComplete="off" />
               </td>
               <td></td>
